6d4365be627f5c16ee47e3475b0001c155e11d60
SirW1zz <108484988+SirW1zz@users.noreply.github.com>
2025-06-25 20:56:30 +0530

    Update main.py
    updated to handle CORS

--- backend/main.py
+++ backend/main.py
@@ -1,8 +1,22 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
 import requests
 
 app = FastAPI()
+
+origins = [
+    "https://moodbotapi.vercel.app",
+    "http://localhost:3000",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 HF_TOKEN = "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"
 HEADERS = {"Authorization": f"Bearer {HF_TOKEN}"}
